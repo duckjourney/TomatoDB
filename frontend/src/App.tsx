60f9afe479a4d100d0693dd19a917a9db934970f
Miguel Leboran <leborandev@gmail.com>
2024-09-03 13:37:34 +0200

    Slightly changed app.tsx structure.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,49 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import Results from "./components/Results.tsx";
-import SearchBar from "./components/SearchBar.tsx";
+import { Box, ChakraProvider } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import Results from './components/Results.tsx';
+import SearchBar from './components/SearchBar.tsx';
+
+const host = import.meta.env.VITE_HOST;
 
 function App() {
-	const [data, setData] = useState([]);
-	const [userInput, setUserInput] = useState("");
+  const query = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  };
+  const [data, setData] = useState([]);
+  const [userInput, setUserInput] = useState('');
 
-	const host = import.meta.env.VITE_HOST;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (userInput !== '') {
+          const response = await fetch(host + userInput);
+          const result = await response.json();
+          setData(result);
+        }
+      } catch (error) {
+        console.error('Error fecthing data', error);
+      }
+    };
+    fetchData();
+  }, [userInput]);
 
-	const query = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setUserInput(e.target.value);
-	};
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				if (userInput !== "") {
-					const response = await fetch(host + userInput);
-					const result = await response.json();
-					setData(result);
-				}
-			} catch (error) {
-				console.error("Error fecthing data", error);
-			}
-		};
-		fetchData();
-	}, [userInput]);
-
-	return (
-		<ChakraProvider>
-			<Box
-				padding="10vh 0 10vh 0"
-				display="flex"
-				flexDirection="column"
-				alignItems="center"
-				justifyContent="flex-start"
-				minHeight="100vh"
-				maxWidth="100vw"
-				bg="#fff"
-			>
-				<SearchBar query={query} />
-				<Results data={data} userInput={userInput} />
-			</Box>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider>
+      <Box
+        padding="10vh 0 10vh 0"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="flex-start"
+        minHeight="100vh"
+        maxWidth="100vw"
+        bg="#fff"
+      >
+        <SearchBar query={query} />
+        <Results data={data} userInput={userInput} />
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
